465b0d0cd39a261e95bfa3d7b3372d50b386c2c6
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-02-10 23:19:04 +0200

    Update Ladder.java
    create setters and getters function for Ladder class

--- Panda2/src/Model/Ladder.java
+++ Panda2/src/Model/Ladder.java
@@ -16,4 +16,54 @@ public class Ladder {
 		super();
 		Length = length;
 	}
+
+	public int getLength() {
+		return Length;
+	}
+
+	public void setLength(int length) {
+		Length = length;
+	}
+
+	public int getXStart() {
+		return XStart;
+	}
+
+	public void setXStart(int xStart) {
+		XStart = xStart;
+	}
+
+	public int getYStart() {
+		return YStart;
+	}
+
+	public void setYStart(int yStart) {
+		YStart = yStart;
+	}
+
+	public int getXEnd() {
+		return XEnd;
+	}
+
+	public void setXEnd(int xEnd) {
+		XEnd = xEnd;
+	}
+
+	public int getYEnd() {
+		return YEnd;
+	}
+
+	public void setYEnd(int yEnd) {
+		YEnd = yEnd;
+	}
+
+	public Random getRand() {
+		return rand;
+	}
+
+	public void setRand(Random rand) {
+		this.rand = rand;
+	}
+	
+	
 }
