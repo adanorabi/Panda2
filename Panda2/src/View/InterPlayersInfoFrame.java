eeca501a734dc27c0968bc5621cd03721b512bd1
adanorabi <adanorabi12@gmail.com>
2024-02-11 00:33:44 +0200

    adding frame InterPlayerInfoFrame
    the frame that the players can choose thier nickNames and colors

---
+++ Panda2/src/View/InterPlayersInfoFrame.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class InterPlayersInfoFrame extends JFrame {
+
+	
+
+}
