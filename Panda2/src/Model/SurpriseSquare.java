61909ed356eb7bb88dbc502c5e20b741a3df32b1
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-10 23:17:09 +0200

    create constructor in SurpriseSquare class
    create a constructor

--- Panda2/src/Model/SurpriseSquare.java
+++ Panda2/src/Model/SurpriseSquare.java
@@ -1,5 +1,17 @@
 package Model;
 
-public class SurpriseSquare {
+import java.util.Random;
+
+public class SurpriseSquare extends Square {
+
+	public SurpriseSquare( int squareRow, int squareCol) {
+		super(squareRow, squareCol);
+		// TODO Auto-generated constructor stub
+	}
+	
+		
+		         
+		
+	}
 
 }
