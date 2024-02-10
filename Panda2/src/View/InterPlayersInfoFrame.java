f04890c5358bb70a5da78c4ddcdf319ccaa8f947
adanorabi <adanorabi12@gmail.com>
2024-02-11 00:35:41 +0200

    Update InterPlayersInfoFrame.java

--- Panda2/src/View/InterPlayersInfoFrame.java
+++ Panda2/src/View/InterPlayersInfoFrame.java
@@ -2,12 +2,56 @@ package View;
 
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+
+import Enum.Levels;
 
 public class InterPlayersInfoFrame extends JFrame {
 
-	
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JRadioButton redRadioButton;
+	private JRadioButton greenRadioButton;
+	private JRadioButton blueRadioButton;
+	private JRadioButton yRadioButton;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTextField textField2;
+	private JRadioButton redRadioButton2;
+	private JRadioButton greenRadioButton2;
+	private JRadioButton blueRadioButton2;
+	private JRadioButton yRadioButton2;
+	private final ButtonGroup buttonGroup2 = new ButtonGroup();
+	private JTextField textField3;
+	private JRadioButton redRadioButton3;
+	private JRadioButton greenRadioButton3;
+	private JRadioButton blueRadioButton3;
+	private JRadioButton yRadioButton3;
+	private final ButtonGroup buttonGroup3 = new ButtonGroup();
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
+	private JButton btnNewButton_1;
+	private JLabel lblNewLabel_8;
+	private JLabel lblNewLabel_9;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_1_4;
+	private JLabel lblNewLabel_10;
+	private JRadioButton redRadioButton2_1;
+	private JRadioButton greenRadioButton2_1;
+	private JRadioButton blueRadioButton2_1;
+	private JRadioButton yRadioButton2_1;
+	private int Num=2;
+	private final ButtonGroup buttonGroup4 = new ButtonGroup();
+	private Levels level;
 
 }
