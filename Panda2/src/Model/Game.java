e75fe13a775973f699ee61cfadf7b4449a45ea80
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-02-10 23:32:35 +0200

    Update Game.java
    build setters and getters function for Game class

--- Panda2/src/Model/Game.java
+++ Panda2/src/Model/Game.java
@@ -27,4 +27,102 @@ public class Game {
 		ColsNum = colsNum;
 		Places=new int[RowsNum][ColsNum];
 	}
+
+	public int getGameId() {
+		return GameId;
+	}
+
+	public void setGameId(int gameId) {
+		GameId = gameId;
+	}
+
+	public Levels getGameLevel() {
+		return GameLevel;
+	}
+
+	public void setGameLevel(Levels gameLevel) {
+		GameLevel = gameLevel;
+	}
+
+	public int getRowsNum() {
+		return RowsNum;
+	}
+
+	public void setRowsNum(int rowsNum) {
+		RowsNum = rowsNum;
+	}
+
+	public int getColsNum() {
+		return ColsNum;
+	}
+
+	public void setColsNum(int colsNum) {
+		ColsNum = colsNum;
+	}
+
+	public int[][] getPlaces() {
+		return Places;
+	}
+
+	public void setPlaces(int[][] places) {
+		Places = places;
+	}
+
+	public int getWinnerId() {
+		return WinnerId;
+	}
+
+	public void setWinnerId(int winnerId) {
+		WinnerId = winnerId;
+	}
+
+	public ArrayList<Integer> getCurentTurn() {
+		return CurentTurn;
+	}
+
+	public void setCurentTurn(ArrayList<Integer> curentTurn) {
+		CurentTurn = curentTurn;
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return Players;
+	}
+
+	public void setPlayers(ArrayList<Player> players) {
+		Players = players;
+	}
+
+	public ArrayList<Snake> getSnakes() {
+		return Snakes;
+	}
+
+	public void setSnakes(ArrayList<Snake> snakes) {
+		Snakes = snakes;
+	}
+
+	public ArrayList<Ladder> getLadders() {
+		return Ladders;
+	}
+
+	public void setLadders(ArrayList<Ladder> ladders) {
+		Ladders = ladders;
+	}
+
+	public Dice getDice() {
+		return dice;
+	}
+
+	public void setDice(Dice dice) {
+		this.dice = dice;
+	}
+
+	public ArrayList<Square> getSquares() {
+		return Squares;
+	}
+
+	public void setSquares(ArrayList<Square> squares) {
+		Squares = squares;
+	}
+	
+	
 }
