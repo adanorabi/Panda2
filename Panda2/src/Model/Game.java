e6b5d363e2dbef190e8fd0911c6ababe9e36b292
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-02-10 23:31:00 +0200

    Update Game.java
    create Game class

--- Panda2/src/Model/Game.java
+++ Panda2/src/Model/Game.java
@@ -1,5 +1,23 @@
 package Model;
 
-public class Game {
+import java.util.ArrayList;
 
+import Enum.Levels;
+
+public class Game {
+	
+	private int GameId;
+	private Levels GameLevel;
+	private int RowsNum;
+	private int ColsNum;
+	private int Places[][];
+	private int WinnerId;
+	private ArrayList<Integer> CurentTurn= new ArrayList<Integer>() ;
+	private ArrayList<Player> Players= new ArrayList<Player>() ;
+	private ArrayList<Snake> Snakes= new ArrayList<Snake>() ;
+	private ArrayList<Ladder> Ladders= new ArrayList<Ladder>() ;
+	private Dice dice;
+	private ArrayList<Square> Squares= new ArrayList<Square>() ;
+
+	
 }
