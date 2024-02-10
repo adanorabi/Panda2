d91de0f15eec79b7594d7b6e36de32b93aaa2161
adanorabi <adanorabi12@gmail.com>
2024-02-11 00:26:47 +0200

    adding start button

--- Panda2/src/View/MainFrame.java
+++ Panda2/src/View/MainFrame.java
@@ -1,43 +1,54 @@
 package View;
-
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1100, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		
 		setContentPane(contentPane);
-		/*hello*/
-	}
+		contentPane.setLayout(null);
 
+		JButton btnNewButton = new JButton("start");/*adding start button*/
+		btnNewButton.addActionListener(this);
+		btnNewButton.setIcon(new ImageIcon(getClass().getResource("/View/img/start.png")));
+		btnNewButton.setBackground(Color.CYAN);
+		btnNewButton.setBounds(427, 266, 125, 130);
+		
+		contentPane.add(btnNewButton);
+
+		
+		
+		
+	}//			
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String s=e.getActionCommand();
+		if(s.equals("start")) {/*start the game and move to levelgame frame*/
+			LevelGame lframe=new LevelGame();
+			this.setVisible(false);
+			lframe.setVisible(true);
+			
+		}
+	}
 }
