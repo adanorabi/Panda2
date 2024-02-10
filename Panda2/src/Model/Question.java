8c37628feaeda6f03e5383ab23fb6d76b7b277f0
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-02-10 23:29:10 +0200

    Update Question.java
    create constructor function for question class

--- Panda2/src/Model/Question.java
+++ Panda2/src/Model/Question.java
@@ -10,5 +10,16 @@ public class Question {
 	private String Content;
 	private ArrayList<String> Answer = new ArrayList<String>(); 
 	private String TrueAnswer;
+	
+	public Question(int questionId, Levels qLevel, String content, ArrayList<String> answer, String trueAnswer) {
+		super();
+		QuestionId = questionId;
+		QLevel = qLevel;
+		Content = content;
+		Answer = answer;
+		TrueAnswer = trueAnswer;
+	}
 
+	
+	
 }
