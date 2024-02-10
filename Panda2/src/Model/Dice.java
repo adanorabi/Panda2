65d64ccb51e5d77cfd938d94121e7c002114366d
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-10 23:09:45 +0200

    adding functions to dice class
    building constructor and getters and setters

--- Panda2/src/Model/Dice.java
+++ Panda2/src/Model/Dice.java
@@ -3,5 +3,16 @@ package Model;
 public class Dice {
 	private int NumOfSides;
 
-	
+	public int getNumOfSides() {
+		return NumOfSides;
+	}
+
+	public void setNumOfSides(int numOfSides) {
+		NumOfSides = numOfSides;
+	}
+
+	public Dice(int numOfSides) {
+		super();
+		NumOfSides = numOfSides;
+	}
 }
