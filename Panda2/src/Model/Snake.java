d7b0e6dae0e4d0d2aa3247f423ab7b62f6e19695
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-02-10 23:03:48 +0200

    Update Snake.java
    Create Snake class

--- Panda2/src/Model/Snake.java
+++ Panda2/src/Model/Snake.java
@@ -1,5 +1,15 @@
 package Model;
 
+import java.util.Random;
+
+import Enum.SnakeColor;
+
 public class Snake {
 
+	private SnakeColor Color;
+	private int XHeadNum;
+	private int YHeadNum;
+	private int XTailNum;
+	private int YTailNum;
+	Random rand = new Random();
 }
