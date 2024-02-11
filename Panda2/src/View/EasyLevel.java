1ed770f277752489d11b3556f196e1994bca4998
Ghaidaaazzam <gidaa.azzam@gmail.com>
2024-02-11 11:00:32 +0200

    easy level board frame
    easy level board frame

--- Panda2/src/View/EasyLevel.java
+++ Panda2/src/View/EasyLevel.java
@@ -32,11 +32,12 @@ public class EasyLevel extends JFrame {
 	 */
 	public EasyLevel() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(400, 100, 1200, 900);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
 	}
 
 }
