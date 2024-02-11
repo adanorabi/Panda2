007295e875af1ee1b85c66c295658fd057c99dd7
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-11 14:47:10 +0200

    building the options of the dice
    filling the sides of the dice according to the game level

--- Panda2/src/Model/Dice.java
+++ Panda2/src/Model/Dice.java
@@ -4,28 +4,73 @@ import Enum.Levels;
 
 public class Dice {
 	private int NumOfSides;
+	private int diceOptions[] ;
 
 	public int getNumOfSides() {
-		return NumOfSides;
+		return this.NumOfSides;
 	}
 
 	public void setNumOfSides(int numOfSides) {
-		NumOfSides = numOfSides;
+		this.NumOfSides = numOfSides;
 	}
 
 	public Dice(Levels GameLevel) {
 		super();
 		if (GameLevel==Levels.Easy) {
 			this.NumOfSides = 8;
+			this.diceOptions=new int[NumOfSides];
+			for(int i=0; i<5;i++) {
+				this.diceOptions[i]=i;
+
+			}
+			this.diceOptions[5]=7; //filling the option of easy question
+			this.diceOptions[6]=8; //filling the option of medium  question
+			this.diceOptions[7]=9; //filling the option of hard  question
+
 		}
 		else if(GameLevel==Levels.Medium) {
-					this.NumOfSides =13 ;
+			this.NumOfSides =13 ;
+			this.diceOptions=new int[NumOfSides];  //filling the places with the number of steps that the player should move
+			for(int i=0; i<7;i++) {
+				this.diceOptions[i]=i;
+
+			}
+			this.diceOptions[7]=7; //filling the option of easy question
+			this.diceOptions[8]=7;
+			this.diceOptions[9]=8; //filling the option of medium question
+			this.diceOptions[10]=8;
+			this.diceOptions[11]=9;//filling the option of hard  question
+			this.diceOptions[12]=9;
+			
+			
+
 		}
 		else {
 			this.NumOfSides =15 ;
+			this.diceOptions=new int[NumOfSides];
+			for(int i=0; i<7;i++) {   //filling the places with the number of steps that the player should move
+				this.diceOptions[i]=i;
+
+			}
+			this.diceOptions[7]=7; //filling the option of easy question
+			this.diceOptions[8]=7;
+			this.diceOptions[9]=8;	//filling the option of medium question
+			this.diceOptions[10]=8;
+			this.diceOptions[11]=9; //filling the option of hard  question
+			this.diceOptions[12]=9;
+			this.diceOptions[13]=9;
+			this.diceOptions[14]=9;
 			
+
+
 		}
 
+
+
 	}
-	
+	public int CheckResultEasy(int diceResult) {
+		;
+
+	}
+
 }
