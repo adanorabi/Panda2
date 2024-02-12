291096102cb90315914dd9834c2d7fb579adf4d8
YomnaZoabi <yomna.adnan2001@gmail.com>
2024-02-12 13:15:01 +0200

    Update SnakeRandomTest.java
    add test for the SnakeRandom function that in Snake class

--- Panda2/unitests/Tests/SnakeRandomTest.java
+++ Panda2/unitests/Tests/SnakeRandomTest.java
@@ -10,44 +10,78 @@ import Model.Snake;
 
 class SnakeRandomTest {
 
-	  @Test
-	    public void testSnackRandom_Easy_Blue() {
-	        Snake snake = new Snake(SnakeColor.Blue);
-	        snake.SnackRandom(Levels.Easy, snake);
-	        assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
-	        assertTrue(snake.getYHeadNum() >= 3 && snake.getYHeadNum() <= 6);
+	@Test
+	public void testSnackRandom_Easy_Blue() {
+		Snake snake = new Snake(SnakeColor.Blue);
+		snake.SnackRandom(Levels.Easy, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
+		assertTrue(snake.getYHeadNum() >= 3 && snake.getYHeadNum() <= 6);
 
-	    }
+	}
 
-	    @Test
-	    public void testSnackRandom_Easy_Green() {
-	        Snake snake = new Snake(SnakeColor.Green);
-	        snake.SnackRandom(Levels.Easy, snake);
-	        assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
-	        assertTrue(snake.getYHeadNum() >= 2 && snake.getYHeadNum() <= 6);
-	    }
+	@Test
+	public void testSnackRandom_Easy_Green() {
+		Snake snake = new Snake(SnakeColor.Green);
+		snake.SnackRandom(Levels.Easy, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
+		assertTrue(snake.getYHeadNum() >= 2 && snake.getYHeadNum() <= 6);
+	}
 
-	    @Test
-	    public void testSnackRandom_Easy_Red() {
-	        Snake snake = new Snake(SnakeColor.Red);
-	        snake.SnackRandom(Levels.Easy, snake);
-	        assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
-	        assertTrue(snake.getYHeadNum() >= 0 && snake.getYHeadNum() <= 6);
-	        assertEquals(snake.getXHeadNum(), snake.getXTailNum());
-	        assertEquals(snake.getYHeadNum(), snake.getYTailNum());
-	    }
+	@Test
+	public void testSnackRandom_Easy_Red() {
+		Snake snake = new Snake(SnakeColor.Red);
+		snake.SnackRandom(Levels.Easy, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
+		assertTrue(snake.getYHeadNum() >= 0 && snake.getYHeadNum() <= 6);
+		assertEquals(snake.getXHeadNum(), snake.getXTailNum());
+		assertEquals(snake.getYHeadNum(), snake.getYTailNum());
+	}
 
-	    @Test
-	    public void testSnackRandom_Easy_Yellow() {
-	        Snake snake = new Snake(SnakeColor.Yellow);
-	        snake.SnackRandom(Levels.Easy, snake);
-	        assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
-	        assertTrue(snake.getYHeadNum() >= 1 && snake.getYHeadNum() <= 6);
-	    }
+	@Test
+	public void testSnackRandom_Easy_Yellow() {
+		Snake snake = new Snake(SnakeColor.Yellow);
+		snake.SnackRandom(Levels.Easy, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 6);
+		assertTrue(snake.getYHeadNum() >= 1 && snake.getYHeadNum() <= 6);
+	}
+
+	@Test
+	public void testSnackRandom_Medium_Blue() {
+		Snake snake = new Snake(SnakeColor.Blue);
+		snake.SnackRandom(Levels.Medium, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 9);
+		assertTrue(snake.getYHeadNum() >= 3 && snake.getYHeadNum() <= 9);
+
+	}
+	@Test
+	public void testSnackRandom_Medium_Green() {
+		Snake snake = new Snake(SnakeColor.Green);
+		snake.SnackRandom(Levels.Medium, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 9);
+		assertTrue(snake.getYHeadNum() >= 2 && snake.getYHeadNum() <= 9);
+	}
+	
+	@Test
+	public void testSnackRandom_Medium_Red() {
+		Snake snake = new Snake(SnakeColor.Red);
+		snake.SnackRandom(Levels.Medium, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 9);
+		assertTrue(snake.getYHeadNum() >= 0 && snake.getYHeadNum() <= 9);
+		assertEquals(snake.getXHeadNum(), snake.getXTailNum());
+		assertEquals(snake.getYHeadNum(), snake.getYTailNum());
+	}
+	
+	@Test
+	public void testSnackRandom_Medium_Yellow() {
+		Snake snake = new Snake(SnakeColor.Yellow);
+		snake.SnackRandom(Levels.Medium, snake);
+		assertTrue(snake.getXHeadNum() >= 0 && snake.getXHeadNum() <= 9);
+		assertTrue(snake.getYHeadNum() >= 1 && snake.getYHeadNum() <= 9);
+	}
 
 
 }
 
 
-  
+
 
