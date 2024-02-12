32c7560d9eba3c81a81cfd0b9c0127e6d0dad52f
Ghaidaaazzam <gidaa.azzam@gmail.com>
2024-02-12 18:22:25 +0200

    Revert "Merge branch 'main' of https://github.com/yara145/Panda2"
    This reverts commit 35d1e15900427c8f1c504766c5958c421713b0bd, reversing
    changes made to 1ba0a8d43c86106ae297bcd517197f255199f1a5.

--- Panda2/src/Model/QuestionSquare.java
+++ Panda2/src/Model/QuestionSquare.java
@@ -3,23 +3,19 @@ package Model;
 import Enum.Levels;
 
 public class QuestionSquare extends Square{
-	private Levels QuesLevel;
-	public static int counter=1;
-	private int QuesSquareId;
+		private Levels QuesLevel;
+		public QuestionSquare(int squareRow, int squareCol, Levels quesLevel) {
+			super( squareRow, squareCol);
+			QuesLevel = quesLevel;
+		}
 
-	public QuestionSquare(int squareRow, int squareCol, Levels quesLevel) {
-		super( squareRow, squareCol);
-		QuesLevel = quesLevel;
-		this.QuesSquareId=this.counter++;
-	}
+		
 
-
-
-
-	public void ChooseQuestion() {  //should be continued!!!
-
-	}
-
-}
-
+		
+		 public void ChooseQuestion() {  //should be continued!!!
+				
+			}
+		 
+	 }
+	
 
