0f9f4fb42c856cb56ec57c92e34fc08c0c0330e4
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-12 15:36:05 +0200

    Update DiceTest.java

--- Panda2/unitests/Tests/DiceTest.java
+++ Panda2/unitests/Tests/DiceTest.java
@@ -1,26 +1,35 @@
 package Tests;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
 import Enum.Levels;
 import Model.Dice;
+
 import java.util.Arrays;
 import java.util.Collection;
+
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+// Annotation indicating that this class is a JUnit test class and will use parameterized tests
 @RunWith(Parameterized.class)
 public class DiceTest {
+
+    // Instance variables to store parameters for testing
     private Levels gameLevel;
     private int expectedNumOfSides;
     private int[] expectedDiceOptions;
 
+    // Constructor to initialize instance variables with parameters from parameterized tests
     public DiceTest(Levels gameLevel, int expectedNumOfSides, int[] expectedDiceOptions) {
         this.gameLevel = gameLevel;
         this.expectedNumOfSides = expectedNumOfSides;
         this.expectedDiceOptions = expectedDiceOptions;
     }
 
+    // Method providing parameters for parameterized tests
     @Parameterized.Parameters
     public static Collection<Object[]> testParameters() {
         return Arrays.asList(new Object[][]{
@@ -30,10 +39,16 @@ public class DiceTest {
         });
     }
 
+    // Actual test method
     @Test
     public void testConstructor() {
+        // Creating an instance of the Dice class with the provided game level
         Dice dice = new Dice(gameLevel);
+        
+        // Asserting that the number of sides matches the expected value
         assertEquals(expectedNumOfSides, dice.getNumOfSides());
+        
+        // Asserting that the dice options match the expected array
         assertArrayEquals(expectedDiceOptions, dice.getDiceOptions());
     }
 }
