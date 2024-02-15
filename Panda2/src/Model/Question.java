7d2255a4be6b4648536a3f546f6f41d456089107
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-15 12:09:53 +0200

    buildin a function of call question and answerFeedback
    this function choose random question , answer feedback check the suitable feedback according to player answer

--- Panda2/src/Model/Question.java
+++ Panda2/src/Model/Question.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.util.ArrayList;
 
+import Controller.SysData;
 import Enum.Levels;
 
 public class Question {
@@ -10,7 +11,7 @@ public class Question {
 	private String Content;
 	private ArrayList<String> Answers;
 	private String TrueAnswer;
-	
+
 	public Question(int questionId, Levels qLevel, String content, ArrayList<String> answer, String trueAnswer) {
 		super();
 		this.QuestionId = questionId;
@@ -67,7 +68,63 @@ public class Question {
 				+ Answers + ", TrueAnswer=" + TrueAnswer + "]";
 	}
 
-	
-	
-	
+	public static Question CallQuestion(Levels questionLevel) { // a function that choose a random question according to rh level
+		if(questionLevel.equals(Levels.Easy)) { //Easy question 
+
+
+			// generating the index using Math.random() 
+			int index = (int)(Math.random() * SysData.EasyQues.size()); 
+
+			System.out.println("Random Question is :"+ SysData.EasyQues.get(index)); //yara!
+			return SysData.EasyQues.get(index); // returning a random easy question
+
+
+		}
+		else if(questionLevel.equals(Levels.Medium)) {//Medium Question
+
+			int index = (int)(Math.random() * SysData.MidQues.size()); 
+
+
+			return SysData.MidQues.get(index); // returning a random medium question
+		} 
+		else {//Hard Question 
+
+			// generating the index using Math.random() 
+			int index = (int)(Math.random() * SysData.HardQues.size()); 
+
+
+			return SysData.HardQues.get(index); // returning a random Hard question
+
+		}
+
+	}
+
+	public static  int answerFeedback(Question question,boolean Answer ) // Function that decide the player movement according to his answers of questions 
+	{
+		if(question.getQLevel().equals(Levels.Easy)) {
+			if( Answer== true) {
+				return 0;
+			}
+			else {
+				return -1;
+			}
+		}
+		else if(question.getQLevel().equals(Levels.Medium)) {
+			if( Answer== true) {
+				return 0;
+			}
+			else {
+				return -2;
+			}
+		}
+		else {
+			if( Answer== true) {   // true answer for Hard question
+				return 1;
+			}
+			else {
+				return -3;
+			}
+		}
+	}
+
 }
