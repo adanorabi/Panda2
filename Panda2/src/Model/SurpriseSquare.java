155440a2640a3fd3952bf04f25eb9c89f048037d
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-16 12:21:02 +0200

    Update SurpriseSquare.java

--- Panda2/src/Model/SurpriseSquare.java
+++ Panda2/src/Model/SurpriseSquare.java
@@ -3,7 +3,7 @@ package Model;
 import java.util.Random;
 
 public class SurpriseSquare extends Square {
-	
+
 	public static int counter=1;
 	private int SurpSquareId;
 
@@ -12,8 +12,8 @@ public class SurpriseSquare extends Square {
 		// TODO Auto-generated constructor stub
 		this.SurpSquareId=this.counter++;
 	}
-	
-	
+
+
 	public int getSurpSquareId() {
 		return SurpSquareId;
 	}
@@ -22,14 +22,15 @@ public class SurpriseSquare extends Square {
 		SurpSquareId = surpSquareId;
 	}
 
+  
 
-
-	public int RandSurprise(int x,int y) { //should be continued!!!
+	public int RandSurprise(int position) { //should be continued!!!
 
 		double randomNum = Math.random(); // Generates a random double between 0.0 (inclusive) and 1.0 (exclusive)
-
+	
 
 		Random random = new Random();
+		
 
 		// Generate a random integer between 0 and 1
 		int randomNumber = random.nextInt(2); // Generates a random integer between 0 (inclusive) and 2 (exclusive)
@@ -38,10 +39,10 @@ public class SurpriseSquare extends Square {
 		int option = randomNumber == 0 ? 1 : 0;
 
 		if (option == 0) {
-			return -10;
+			return position-10;
 		}
 		else {
-			return 10;
+			return position+10;
 		}
 
 
