6075daa2605ffb1992c1494934ec7d8c14c72bed
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-17 20:18:37 +0200

    Create a UniTest  for reading json file
    this test check ig the json file loaded correctly and saved in the suitable data structure

---
+++ Panda2/unitests/Tests/ReadJsonTest.java
@@ -0,0 +1,56 @@
+package Tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.junit.Before;
+import org.junit.Test;
+
+import Controller.SysData;
+import Enum.Levels;
+import Model.Question;
+
+public class ReadJsonTest {
+
+    private SysData sysData;
+
+    @Before
+    public void setUp() {
+        sysData = new SysData();
+    }
+
+    @Test
+    public void testUploadQuestions() {
+        SysData.UploadQuestions();
+
+        // Test if questions are uploaded correctly
+        ArrayList<Question> easyQuestions = SysData.EasyQues;
+        ArrayList<Question> mediumQuestions = SysData.MidQues;
+        ArrayList<Question> hardQuestions = SysData.HardQues;
+
+        // Test if questions are not null
+        assertNotNull("Easy questions list is null", easyQuestions);
+        assertNotNull("Medium questions list is null", mediumQuestions);
+        assertNotNull("Hard questions list is null", hardQuestions);
+
+        // Test if questions are uploaded with correct difficulty level
+        for (Question question : easyQuestions) {
+            assertEquals("Question difficulty is not easy", Levels.Easy, question.getQLevel());
+        }
+
+        for (Question question : mediumQuestions) {
+            assertEquals("Question difficulty is not medium", Levels.Medium, question.getQLevel());
+        }
+
+        for (Question question : hardQuestions) {
+            assertEquals("Question difficulty is not hard", Levels.Hard, question.getQLevel());
+        }
+    }
+}
