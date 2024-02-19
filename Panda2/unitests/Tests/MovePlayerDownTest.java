cbbc41ab910f3cd53454677c6ca2908a2ca05f90
Yara Ghaben <yaraghaben14@gmail.com>
2024-02-19 03:52:55 +0200

    Update MovePlayerDownTest.java

--- Panda2/unitests/Tests/MovePlayerDownTest.java
+++ Panda2/unitests/Tests/MovePlayerDownTest.java
@@ -25,53 +25,7 @@ class MovePlayerDownTest {
 	        assertEquals(1, result[1]);
 	    }
 
-	  @Test
-	    public void testMovePlayerDown_PlayerAtGreenHead() {
-	        Snake snake = new Snake(SnakeColor.Green);
-	        // Set snake's head position
-	        snake.setXHeadNum(3);
-	        snake.setYHeadNum(4);
-	        // Set snake's tail position
-	        snake.setXTailNum(3);
-	        snake.setYTailNum(2);
-
-	        int[] result = snake.MovePlayerDown(3, 4);
-
-	        assertEquals(3, result[0]);
-	        assertEquals(2, result[1]);
-	    }
-
-	  @Test
-	    public void testMovePlayerDown_PlayerAtRedHead() {
-	        Snake snake = new Snake(SnakeColor.Red);
-	        // Set snake's head position
-	        snake.setXHeadNum(3);
-	        snake.setYHeadNum(4);
-	        // Set snake's tail position
-	        snake.setXTailNum(0);
-	        snake.setYTailNum(0);
-
-	        int[] result = snake.MovePlayerDown(3, 4);
-
-	        assertEquals(0, result[0]);
-	        assertEquals(0, result[1]);
-	    }
 	  
-	  @Test
-	    public void testMovePlayerDown_PlayerAtYellowHead() {
-	        Snake snake = new Snake(SnakeColor.Yellow);
-	        // Set snake's head position
-	        snake.setXHeadNum(3);
-	        snake.setYHeadNum(4);
-	        // Set snake's tail position
-	        snake.setXTailNum(3);
-	        snake.setYTailNum(3);
-
-	        int[] result = snake.MovePlayerDown(3, 4);
-
-	        assertEquals(3, result[0]);
-	        assertEquals(3, result[1]);
-	    }
 
 }
 
