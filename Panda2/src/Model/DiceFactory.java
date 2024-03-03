f5c2c4220e6c29d3a97e93fb5d150eafee63e99d
Yara Ghaben <yaraghaben14@gmail.com>
2024-03-03 23:20:27 +0200

    Create DiceFactory.java
    creating dice factory that create dice according to game level

---
+++ Panda2/src/Model/DiceFactory.java
@@ -0,0 +1,22 @@
+//package Model;
+//
+//import Enum.Levels;
+//public class DiceFactory {
+//
+//
+//	public static Dice createDice(Levels diceLevel) {  // a factory that create a dice
+//
+//		switch(diceLevel) {
+//		case Easy:
+//			return new EasyDice(diceLevel);
+//		case Medium:
+//			return new MidDice(diceLevel);
+//		case Hard:
+//			return new HardDice(diceLevel);
+//		default: 
+//			return null;
+//
+//		}
+//
+//	}
+//}
