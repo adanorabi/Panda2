c8195eaf2d63653238968515347c0a19418bb6c0
Yara Ghaben <yaraghaben14@gmail.com>
2024-03-09 01:33:30 +0200

    update audio

--- Panda2/src/Model/PlayAudio.java
+++ Panda2/src/Model/PlayAudio.java
@@ -6,7 +6,8 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
-public class PlayerAudio {
+public class PlayAudio {
+	public static Clip hpClip ;
 	public static void playVictorySound() {
         try {
             File audioFile = new File("/Audio/victory.wav"); // Adjust the file path accordingly
@@ -22,26 +23,9 @@ public class PlayerAudio {
 		try {
 			File audioFile = new File("src/Model/Audio/MainPage.wav"); // Adjust the file path accordingly
 			AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-			Clip clip = AudioSystem.getClip();
-			clip.open(audioStream);
-
-
-			while(!response.equals("Q")) {
-			
-
-				switch(response) {
-				case ("P"): clip.start();
-				break;
-				case ("S"): clip.stop();
-				break;
-				case ("R"): clip.setMicrosecondPosition(0);
-				break;
-				case ("Q"): clip.close();
-				break;
-				default: System.out.println("Not a valid response");
-				}
-			}
-			clip.start();
+			hpClip = AudioSystem.getClip();
+			hpClip.open(audioStream);
+			hpClip.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
