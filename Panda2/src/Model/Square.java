77fd608424db3ae801c6ba081e80b4b0c2da9ce8
Yara Ghaben <yaraghaben14@gmail.com>
2024-03-10 20:53:59 +0200

    Update Square.java

--- Panda2/src/Model/Square.java
+++ Panda2/src/Model/Square.java
@@ -1,18 +1,17 @@
 package Model;
-
+// -----------Square class a parent class of SurpriseSquare and question Square BY Yara--------------
 public class Square {
  
-
+//normal square attributes 
 	private int SquareRow;
 	private int SquareCol;
-
+//normal square constructor
 	public Square( int squareRow, int squareCol) {
 		super();
 		SquareRow = squareRow;
 		SquareCol = squareCol;
 	}
-
-
+//getters and setters
 	public int getSquareRow() {
 		return SquareRow;
 	}
